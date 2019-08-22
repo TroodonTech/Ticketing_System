481e720071bc96eca82546b997615c7d26bbd368
Aswathy Mohan L <aswathymohan.troodon@outlook.com>
2019-08-23 00:05:00 +0530

    dfv

--- backend/server.js
+++ backend/server.js
@@ -41,6 +41,10 @@ function supportCrossOriginScript(req, res, next) {
     next();
 }
 
+
+////////////////////code by aswathy starts/////////////////////////////////////////////////////
+
+
 var user_return = '';
 var employeeid_return = '';
 app.options('/authenticate', supportCrossOriginScript);
@@ -119,46 +123,6 @@ app.get('/getpriority', function (req, res) {
     });
 
 });
-////////////////////raima
-app.get('/getuserroletype', function (req, res) {
-
-    connection.query('call usp_getuserroletype()', function (err, rows) {
-        if (err) {
-            console.log("Problem with MySQL" + err);
-        }
-        else {
-            console.log("addnamess  is  " + JSON.stringify(rows[0]));
-
-            res.end(JSON.stringify(rows[0]));
-        }
-        res.end();
-    });
-
-});
-app.post('/addemployee', supportCrossOriginScript, function (req, res) {
-    var FirstName = req.body.FirstName;
-    var LastName = req.body.LastName;
-    var MiddleName = req.body.MiddleName;
-    var Address = req.body.Address;
-    var Phone = req.body.Phone;
-    var MiddleName = req.body.EmailID;
-    var UserRoleType=req.body.UserRoleType;
-
-    connection.query('set @FirstName=?;set @LastName=?;set @MiddleName=?;set @Address=?;set @Phone=?;set @EmailID=?;set @UserRoleType=?;call usp_submitissue(@FirstName,@LastName,@MiddleName,@Address,@Phone,@EmailID,@UserRoleType)', [FirstName, LastName, MiddleName,Address,Phone,EmailID,UserRoleType], function (err, rows) {
-        if (err) {
-            console.log("Problem with MySQL" + err);
-        }
-        else {
-            console.log("NewItem  is  " + JSON.stringify(rows[6]));
-
-            res.end(JSON.stringify(rows[3]));
-        }
-        res.end();
-    });
-
-});
-
-//////////////////////////////
 
 app.options('/submitIssue', supportCrossOriginScript);
 app.post('/submitIssue', supportCrossOriginScript, function (req, res) {
@@ -341,105 +305,50 @@ app.get('/purchaseapi', function (req, res) {
     });
 
 });
+//////////////////////////////code by aswathy ends//////////////////////////////////////////
 
-app.get('/removeapi', function (req, res) {
-    var cartid = url.parse(req.url, true).query['idcart'];
-    connection.query('set @cartid=?; call usp_removecart(@cartid)', [cartid], function (err, rows) {
+
+
+
+////////////////////code by raima starts/////////////////////////////////////////////////////
+
+
+app.get('/getuserroletype', function (req, res) {
+
+    connection.query('call usp_getuserroletype()', function (err, rows) {
         if (err) {
             console.log("Problem with MySQL" + err);
         }
         else {
-            console.log("prodnames  is  " + JSON.stringify(rows[1]));
+            console.log("addnamess  is  " + JSON.stringify(rows[0]));
 
-            res.end(JSON.stringify(rows[1]));
+            res.end(JSON.stringify(rows[0]));
         }
         res.end();
     });
 
 });
-// app.post('/updateapi', function (req, res) {
-//     var editkey = req.body.edkey;
-//     var editname = req.body.edname;
-//     var editage = req.body.edage;
-//     var editaddr = req.body.edaddr;
-//     var editphone = req.body.edphone;
-//     var editemail = req.body.edemail;
-//     //  var Mark = url.parse(req.url, true).query['Mark'];
-//     //console.log("/insertItem@prid"+prid+'@bno'+bno+'@edate'+edate+'@qn'+qn+'@price'+price);
-//     connection.query('set @editkey=?;set @editname=?; set @editage=?; set @editaddr=?; set @editphone=?; set @editemail=?; call usp_editdetails(@editkey,@editname,@editage,@editaddr,@editphone,@editemail)', [editkey,editname, editage, editaddr, editphone, editemail], function (err, rows) {
-//         if (err) {
-//             console.log("Problem with MySQL" + err);
-//         }
-//         else {
-//             console.log("NewItem  is  " + JSON.stringify(rows[5]));
+app.post('/addemployee', supportCrossOriginScript, function (req, res) {
+    var FirstName = req.body.FirstName;
+    var LastName = req.body.LastName;
+    var MiddleName = req.body.MiddleName;
+    var Address = req.body.Address;
+    var Phone = req.body.Phone;
+    var MiddleName = req.body.EmailID;
+    var UserRoleType=req.body.UserRoleType;
 
-//             res.end(JSON.stringify(rows[6]));
-//         }
-//         res.end();
-//     });
-
-// });
-app.get('/removeproapi', function (req, res) {
-    var prodid = url.parse(req.url, true).query['productid'];
-    connection.query('set @prodid=?; call usp_removeproduct(@prodid)', [prodid], function (err, rows) {
+    connection.query('set @FirstName=?;set @LastName=?;set @MiddleName=?;set @Address=?;set @Phone=?;set @EmailID=?;set @UserRoleType=?;call usp_submitissue(@FirstName,@LastName,@MiddleName,@Address,@Phone,@EmailID,@UserRoleType)', [FirstName, LastName, MiddleName,Address,Phone,EmailID,UserRoleType], function (err, rows) {
         if (err) {
             console.log("Problem with MySQL" + err);
         }
         else {
-            console.log("prodnames  is  " + JSON.stringify(rows[1]));
+            console.log("NewItem  is  " + JSON.stringify(rows[6]));
 
-            res.end(JSON.stringify(rows[1]));
+            res.end(JSON.stringify(rows[3]));
         }
         res.end();
     });
 
 });
 
-
-//githubproject
-app.get('/push', function (req, res) {
-
-    var message = new gcm.Message({
-        data: { key1: 'hello' },
-        notification: {
-            title: 'SPECOZ Offers1',
-            body: 'body_data'
-        }
-    });
-
-    // Set up the sender with you API key, prepare your recipients' registration tokens.
-    var sender = new gcm.Sender('AAAAsi_G1xw:APA91bHzbxJ7AN0Wb7eUQLB4lydiO21pVaG4FSfF2WbnLet2PDP1sll8npOLEMnVN2jd2_R-R7kDy9kv6b8DaWAt8v5Xwy95UzI0YFV58DlpjboOb_p2fCYDdCi3oU91zba01zwXlkMj');
-    sender.send(message, 'eL5VFGcwZhY:APA91bGw6X-VQ7dl1xvBEQxVm8qaeP8o4DCvzzuATTGYpd1Oq6UCefmYtQjmKQfNZq4Sm2eMgECwpOC8XCPxUBvnR890fWf7EafVSyp6CFyXVtACp6XA8tJyQThCAjas8P5BiVtcPeqp', function (err, response) {
-        if (err) {
-            console.error("Error:", err);
-        }
-
-        else console.log("Response:", response);
-        res.send(response);
-    });
-
-});
-
-//pushnotification
-app.get('/push', function (req, res) {
-
-    var message = new gcm.Message({
-        data: { key1: 'hello' },
-        notification: {
-            title: 'SPECOZ Offers1',
-            body: 'body_data'
-        }
-    });
-
-    // Set up the sender with you API key, prepare your recipients' registration tokens.
-    var sender = new gcm.Sender('AAAABwyNNQA:APA91bHnjLAYp_aZjQiu3Sv6gelEdJlyEdkSAsgmq4Ojewo86HRwxmLiDAt69U0QD5UeObRqkOpcvbyxkGZY4Q09iFAK0W9HgBYtmYmizH3cgc6Dq3kpU_F3Ff4giVx0YdCTu2eJu7lq');
-    sender.send(message, 'dFYCS-mDsow%3AAPA91bExHIMnvh6QerfLkG8gGCwUDlGq7nqOXI2MJjdrh3T1yVkuu7UJRc3HzaUIaZgwsOLs7bmal-nmelylpc93t3c2q2_hRJuV_C2_7hDY_Pi95sWp7pa98TQEMIdeqVrx-7lVaH1b', function (err, response) {
-        if (err) {
-            console.error("Error:", err);
-        }
-
-        else console.log("Response:", response);
-        res.send(response);
-    });
-
-});
+//////////////////////////////code by raima ends//////////////////////////////////////////
