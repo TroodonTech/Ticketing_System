7dad12584ef2956cf043ff1cd60d160f25c6d0e5
Aswathy Mohan L <aswathymohan.troodon@outlook.com>
2019-08-22 18:02:10 +0530

    REPORT ISSUE

--- src/app/client/client.module.ts
+++ src/app/client/client.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ViewIssuesComponent } from './view-issues/view-issues.component';
+import { HistoryDetailsComponent } from './history-details/history-details.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ViewIssuesComponent, HistoryDetailsComponent],
   imports: [
     CommonModule
   ]
